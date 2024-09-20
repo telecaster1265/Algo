c120bd2a2b809a9325cd4d9a216ea27a94f5ccd2
telecaster1265 <124582446+telecaster1265@users.noreply.github.com>
2024-09-21 00:26:10 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println( s + "??!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        System.out.println(s + "??!");
     }
 }
