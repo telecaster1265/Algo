6f50a0376636d2b0dbd38bdf0418dad58a5e8f3f
telecaster1265 <124582446+telecaster1265@users.noreply.github.com>
2024-09-23 02:22:13 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,9 +1,10 @@
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
-    public static void main(String[] args)  {
-       Scanner sc = new Scanner(System.in);
-       int score = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     int score = Integer.parseInt(br.readLine());
 
        if (score >= 90) {
            System.out.println("A");
