29a8e1f1b2e141bab990b695f85c5f9c99662f21
telecaster1265 <124582446+telecaster1265@users.noreply.github.com>
2024-09-23 11:45:55 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.print((a % 4 == 0)?((a % 400 == 0))? "1" :(a % 100 == 0) ? "0":"1":"0");
+    }
+}
