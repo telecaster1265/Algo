73c3809158fcfd368423c852dfca6ce64462a936
telecaster1265 <124582446+telecaster1265@users.noreply.github.com>
2024-11-07 20:22:03 +0900

    [Bronze V] Title: 사파리월드, Time: 168 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+               
+        long n = sc.nextInt();
+        long m = sc.nextInt();
+        System.out.println(Math.abs(n - m));
+    }
+}
