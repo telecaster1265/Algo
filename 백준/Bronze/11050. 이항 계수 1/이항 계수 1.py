1c87ce933a3396496e2599259c38d2e03b7b2dd5
telecaster1265 <124582446+telecaster1265@users.noreply.github.com>
2025-04-03 21:34:09 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,4 +1,10 @@
-import math
-
 n, k = map(int, input().split())
-print(math.comb(n, k))
+
+dp = [0] * (k + 1)
+dp[0] = 1
+
+for i in range(1, n + 1):
+    for j in range(min(i, k), 0, -1):
+        dp[j] += dp[j - 1]
+
+print(dp[k])
