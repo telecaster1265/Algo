b2a2c89954210666366ac514abfb14c045a7a241
telecaster1265 <124582446+telecaster1265@users.noreply.github.com>
2025-04-25 00:08:03 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,17 @@
+grades = {
+  'A+':4.5, 'A0':4.0, 'B+':3.5, 'B0':3.0, 'C+':2.5, 'C0':2.0, 'D+':1.5, 'D0':1.0, 'F':0.0
+}
+total_score = 0.0
+total_credits = 0.0
+
+for _ in range(20):
+    subject, credit, grade = input().split()
+    credit = float(credit)
+    
+    if grade == 'P':
+        continue
+        
+    total_score += credit * grades[grade]
+    total_credits += credit
+    
+print(f"{total_score / total_credits:.6f}")
