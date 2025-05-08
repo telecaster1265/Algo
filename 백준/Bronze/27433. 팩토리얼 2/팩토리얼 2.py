d122ce25f8e5cc3a6c34a1f51fd9705ab247f5a4
telecaster1265 <124582446+telecaster1265@users.noreply.github.com>
2025-05-08 23:55:47 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(n))
