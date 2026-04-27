c2782bddf24bbb883344dc8e46c91ea090ec510d
telecaster1265 <124582446+telecaster1265@users.noreply.github.com>
2026-04-27 16:16:39 +0900

    [Silver IV] Title: 동전 0, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,25 +1,20 @@
-import java.util.*;
-
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); 
-        int K = sc.nextInt(); 
-        
-        int[] coin = new int[N];
-        
-        for(int i = 0; i < N; i++) {
-            coin[i] = sc.nextInt(); 
-        }
-
-        int count = 0;
-
-        for(int i = N - 1; i >= 0; i--) {
-            if(coin[i] <= K) {
-                count += (K / coin[i]);
-                K = K % coin[i];
-            }
-        }
-        System.out.println(count);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < N; i++) {
+      A[i] = sc.nextInt();
     }
+    int count = 0;
+    for (int i = N - 1; i >= 0; i--) {
+      if (A[i] <= K) { 
+        count += (K / A[i]);
+        K = K % A[i]; 
+      }
+    }
+    System.out.println(count);
+  }
 }
