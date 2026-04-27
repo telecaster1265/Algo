8f26ca35cb2c40721b1354621e37d73d8bd4c1a0
telecaster1265 <124582446+telecaster1265@users.noreply.github.com>
2026-04-27 15:28:46 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2372 ms, Memory: 340672 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,22 +1,50 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 public class Main {
+    public static int[] A;
+    public static long result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int[] count = new int[10001];
-
-        for (int i = 0; i < n; i++) {
-            count[Integer.parseInt(br.readLine())]++;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= 10000; i++) {
-            while (count[i]-- > 0) {
-                sb.append(i).append("\n");
-            }
+        br.close();
+        Radix_Sort(A, 5);
+        for (int i = 0; i < N; i++) {
+            bw.write(A[i] + "\n");
         }
-        System.out.print(sb);
+        bw.flush();
+        bw.close();
     }
+
+    public static void Radix_Sort(int[] A, int max_size) {
+        int[] output = new int[A.length];
+        int jarisu = 1;
+        int count = 0;
+        while (count != max_size)
+        {
+            int[] bucket = new int[10];
+            for (int i = 0; i < A.length; i++) {
+                bucket[(A[i] / jarisu) % 10]++; 
+            }
+            for (int i = 1; i < 10; i++) { 
+                bucket[i] += bucket[i - 1];
+            }
+            for (int i = A.length - 1; i >= 0; i--) { 
+                output[bucket[(A[i] / jarisu % 10)] - 1] = A[i];
+                bucket[(A[i] / jarisu) % 10]--;
+            }
+            for (int i = 0; i < A.length; i++) {
+                A[i] = output[i]; 
+            }
+            jarisu = jarisu * 10; 
+            count++;
+        }
+    };
 }
